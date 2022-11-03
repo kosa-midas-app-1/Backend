7bb29d1982082aa7dc4ef729e10f2bd3680703af
junseo <ljchan71@gmail.com>
2022-11-03 22:13:12 +0900

    feat: Authority

---
+++ Jobda/src/main/kotlin/com/example/jobda/domain/auth/type/Authority.kt
@@ -0,0 +1,13 @@
+package com.example.jobda.domain.auth.type
+
+/**
+ *
+ * Authority
+ *
+ * @author ljcha
+ * @date 2022-11-03
+ * @version 1.0.0
+ **/
+enum class Authority {
+    MANAGER, STAFF
+}
