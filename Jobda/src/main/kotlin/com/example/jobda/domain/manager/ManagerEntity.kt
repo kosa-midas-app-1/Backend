6e93aa18da6405a854035b7cfab19b5cff0852a4
junseo <ljchan71@gmail.com>
2022-11-03 21:15:57 +0900

    feat: ManagerEntity

---
+++ Jobda/src/main/kotlin/com/example/jobda/domain/manager/ManagerEntity.kt
@@ -0,0 +1,41 @@
+package com.example.jobda.domain.manager
+
+import com.example.jobda.common.BaseUUIDEntity
+import com.example.jobda.domain.company.CompanyEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+/**
+ *
+ * ManagerEntity
+ *
+ * @author ljcha
+ * @date 2022-11-03
+ * @version 1.0.0
+ **/
+@Entity
+@Table(name = "tbl_manager")
+class ManagerEntity(
+
+    @field:Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    val email: String,
+
+    @field:Column(columnDefinition = "VARCHAR(255)", nullable = false)
+    val password: String,
+
+    @field:Column(columnDefinition = "VARCHAR(11)", nullable = false)
+    val phoneNumber: String,
+
+    @field:Column(columnDefinition = "VARCHAR(5)", nullable = false)
+    val name: String,
+
+    @field:ManyToOne(fetch = FetchType.LAZY)
+    @field:JoinColumn(columnDefinition = "VARCHAR(16)", nullable = false)
+    val companyEntity: CompanyEntity?
+
+) : BaseUUIDEntity() {
+}
