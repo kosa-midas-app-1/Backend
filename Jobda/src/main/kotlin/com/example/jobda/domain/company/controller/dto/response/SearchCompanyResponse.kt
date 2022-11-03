a3e79ae726835d25a63eb2db66d6ed184b45e2de
junseo <ljchan71@gmail.com>
2022-11-04 03:01:09 +0900

    feat: searchCompany api

---
+++ Jobda/src/main/kotlin/com/example/jobda/domain/company/controller/dto/response/SearchCompanyResponse.kt
@@ -0,0 +1,19 @@
+package com.example.jobda.domain.company.controller.dto.response
+
+/**
+ *
+ * SearchCompanyResponse
+ *
+ * @author ljcha
+ * @date 2022-11-04
+ * @version 1.0.0
+ **/
+data class SearchCompanyResponse(
+    val companies: List<CompanyElement>
+) {
+    data class CompanyElement(
+        val name: String,
+        val managerName: String,
+        val managerPhoneNumber: String
+    )
+}
---
+++ Jobda/src/main/kotlin/com/example/jobda/domain/company/service/SearchCompanyService.kt
@@ -0,0 +1,34 @@
+package com.example.jobda.domain.company.service
+
+import com.example.jobda.domain.company.controller.dto.response.SearchCompanyResponse
+import com.example.jobda.domain.company.repository.CompanyRepository
+import org.springframework.stereotype.Service
+
+/**
+ *
+ * SearchCompanyService
+ *
+ * @author ljcha
+ * @date 2022-11-04
+ * @version 1.0.0
+ **/
+@Service
+class SearchCompanyService(
+    private val companyRepository: CompanyRepository
+) {
+
+    fun execute(name: String): SearchCompanyResponse {
+
+        val companies = companyRepository.findByNameContaining(name).map {
+            val manager = it.managerEntity!!
+
+            SearchCompanyResponse.CompanyElement(
+                name = it.name,
+                managerName = manager.name,
+                managerPhoneNumber = manager.phoneNumber
+            )
+        }
+
+        return SearchCompanyResponse(companies)
+    }
+}
