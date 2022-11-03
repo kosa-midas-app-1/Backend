5c8f92ea0d72abb7eb31c9f2b422e4591621a212
junseo <ljchan71@gmail.com>
2022-11-03 21:43:47 +0900

    feat: Status

---
+++ Jobda/src/main/kotlin/com/example/jobda/domain/staff/type/Status.kt
@@ -0,0 +1,13 @@
+package com.example.jobda.domain.staff.type
+
+/**
+ *
+ * Status
+ *
+ * @author ljcha
+ * @date 2022-11-03
+ * @version 1.0.0
+ **/
+enum class Status {
+    NO_DATA, WORKING, LEAVE_WORK, LEAVE_EARLY, WORK_HOME
+}
