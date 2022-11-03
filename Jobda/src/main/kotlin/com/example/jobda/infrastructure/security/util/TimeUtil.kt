80f9642367553e0d3090e9c140bf16359199dbc1
junseo <ljchan71@gmail.com>
2022-11-04 04:06:26 +0900

    feat: timeUtil

---
+++ Jobda/src/main/kotlin/com/example/jobda/infrastructure/security/util/TimeUtil.kt
@@ -0,0 +1,21 @@
+package com.example.jobda.infrastructure.security.util
+
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+/**
+ *
+ * TimeUtil
+ *
+ * @author ljcha
+ * @date 2022-11-04
+ * @version 1.0.0
+ **/
+@Component
+class TimeUtil {
+
+    fun betweenForMinutes(startAt: LocalDateTime, endAt: LocalDateTime): Long {
+        return ChronoUnit.HOURS.between(startAt, endAt) * 60 + ChronoUnit.MINUTES.between(startAt, endAt)
+    }
+}
