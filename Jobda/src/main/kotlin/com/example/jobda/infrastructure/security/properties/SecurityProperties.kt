44ccc77e856015381d76c51dd17b09bf8d1daaed
junseo <ljchan71@gmail.com>
2022-11-04 04:45:43 +0900

    feat: SecurityProperties

--- Jobda/src/main/kotlin/com/example/jobda/infrastructure/security/properties/SecurityProperties.kt
+++ Jobda/src/main/kotlin/com/example/jobda/infrastructure/security/properties/SecurityProperties.kt
@@ -1,7 +1,10 @@
 package com.example.jobda.infrastructure.security.properties
 
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import java.security.Key
 import java.util.Base64
 
 /**
@@ -10,7 +13,7 @@ import java.util.Base64
  *
  * @author ljcha
  * @date 2022-11-03
- * @version 1.0.0
+ * @version 2.0.0
  **/
 @ConfigurationProperties(prefix = "jwt")
 @ConstructorBinding
@@ -19,7 +22,10 @@ class SecurityProperties(
     accessExp: Int,
     refreshExp: Int
 ) {
-    val secretKey: String = Base64.getEncoder().encodeToString(secretKey.toByteArray())
+    val key: Key by lazy {
+        val secretKey: String = Base64.getEncoder().encodeToString(secretKey.toByteArray())
+        Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey))
+    }
     val accessExp: Int = accessExp * 1000
     val refreshExp: Int = refreshExp * 1000
 }
