667297c0f8b76b3c5332beef264694c6ba209026
junseo <ljchan71@gmail.com>
2022-11-03 23:53:05 +0900

    feat: ErrorResponse

---
+++ Jobda/src/main/kotlin/com/example/jobda/infrastructure/error/response/ErrorResponse.kt
@@ -0,0 +1,14 @@
+package com.example.jobda.infrastructure.error.response
+
+/**
+ *
+ * ErrorResponse
+ *
+ * @author ljcha
+ * @date 2022-11-03
+ * @version 1.0.0
+ **/
+data class ErrorResponse(
+    val status: Int,
+    val message: String
+)
