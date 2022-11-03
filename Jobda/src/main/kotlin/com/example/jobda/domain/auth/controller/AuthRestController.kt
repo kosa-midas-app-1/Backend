b65937625f398f55018cce43540e5603f36d6bfb
junseo <ljchan71@gmail.com>
2022-11-04 01:40:52 +0900

    chore: AuthRestController

--- Jobda/src/main/kotlin/com/example/jobda/domain/auth/controller/AuthRestController.kt
+++ Jobda/src/main/kotlin/com/example/jobda/domain/auth/controller/AuthRestController.kt
@@ -8,10 +8,12 @@ import com.example.jobda.domain.auth.service.ReissueTokenService
 import com.example.jobda.domain.auth.service.SignInService
 import com.example.jobda.domain.auth.service.SignUpManagerService
 import com.example.jobda.domain.auth.service.SignUpStaffService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -21,7 +23,7 @@ import javax.validation.Valid
  *
  * @author ljcha
  * @date 2022-11-04
- * @version 1.0.0
+ * @version 1.1.0
  **/
 @RestController
 class AuthRestController(
@@ -31,16 +33,19 @@ class AuthRestController(
     private val reissueTokenService: ReissueTokenService
 ) {
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/managers")
     fun singUpManager(@RequestBody @Valid request: SignUpManagerRequest): TokenResponse {
         return singUpManagerService.execute(request)
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/staff")
     fun signUpStaff(@RequestBody @Valid request: SignUpStaffRequest): TokenResponse {
         return singUpStaffService.execute(request)
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun signIn(@RequestBody @Valid request: SignInRequest): TokenResponse {
         return signInService.execute(request)
