b252a26dbabec8d2e0566146b1a9bbf5f395ca9f
junseo <ljchan71@gmail.com>
2022-11-03 21:44:09 +0900

    feat: WorkSystem

---
+++ Jobda/src/main/kotlin/com/example/jobda/domain/company/type/WorkSystem.kt
@@ -0,0 +1,13 @@
+package com.example.jobda.domain.company.type
+
+/**
+ *
+ * WorkSystem
+ *
+ * @author ljcha
+ * @date 2022-11-03
+ * @version 1.0.0
+ **/
+enum class WorkSystem {
+    FLEXTIME, ALTERNATIVE
+}
