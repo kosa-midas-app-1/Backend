8496997ca776526d86bf2ec193ff5d7073fe69f8
junseo <ljchan71@gmail.com>
2022-11-04 01:39:05 +0900

    feat: SecurityConfig

--- Jobda/src/main/kotlin/com/example/jobda/infrastructure/security/config/SecurityConfig.kt
+++ Jobda/src/main/kotlin/com/example/jobda/infrastructure/security/config/SecurityConfig.kt
@@ -1,10 +1,12 @@
 package com.example.jobda.infrastructure.security.config
 
+import com.example.jobda.domain.auth.type.Authority
 import com.example.jobda.infrastructure.config.FilterConfig
 import com.example.jobda.infrastructure.security.jwt.JwtParser
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -16,7 +18,7 @@ import org.springframework.security.web.SecurityFilterChain
  *
  * @author ljcha
  * @date 2022-11-03
- * @version 2.0.0
+ * @version 3.0.0
  **/
 @Configuration
 class SecurityConfig(
@@ -37,6 +39,8 @@ class SecurityConfig(
         http
             .authorizeRequests()
             .antMatchers("/auth/**").permitAll()
+            .antMatchers(HttpMethod.POST, "/managers").hasAuthority(Authority.MANAGER.name)
+            .antMatchers(HttpMethod.GET, "/managers").hasAuthority(Authority.MANAGER.name)
 
         http
             .apply(FilterConfig(jwtParser, objectMapper))
